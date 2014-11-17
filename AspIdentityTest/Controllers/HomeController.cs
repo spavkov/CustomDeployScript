b47f4d4319481252964f2cb012533042c148b09f
spavkov <spavkov@gmail.com>
2014-11-17 12:29:02 +0100

    adding tracing...

--- AspIdentityTest/Controllers/HomeController.cs
+++ AspIdentityTest/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,7 @@ namespace AspIdentityTest.Controllers
     {
         public ActionResult Index()
         {
+            Trace.WriteLine("Index");
             return View();
         }
 
